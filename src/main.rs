5eb0b3ef16e614f625bcc7f7d34ab51bf766ad94
Ryan Avella <ryanavella@users.noreply.github.com>
2023-09-03 15:28:09 -0700

    Removing unnecessary Vec allocation which can be better expressed in a static array. Trying to avoid too many magic numbers when determining the RNG mask. Misc cleanup.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@ use std::num::NonZeroU8;
 
 use clap::Parser;
 
-fn wordlist() -> Vec<&'static str> {
-    static WORDLIST: &str = include_str!("eff_large_wordlist.txt");
-
-    WORDLIST.lines().map(str::trim).collect()
-}
+mod wordlist;
+use wordlist::WORDS;
 
 /// Generate a random passphrase
 #[derive(Parser)]
@@ -19,27 +16,29 @@ struct Args {
 fn main() {
     use rand::rngs::OsRng;
 
-    let wordlist = wordlist();
-    let args = Args::parse();
+    const WORDS_LEN: usize = 7776;
+    assert_eq!(WORDS.len(), WORDS_LEN);
+    const MASK: u16 = WORDS_LEN.next_power_of_two() as u16 - 1;
+
+    let Args { num_words } = Args::parse();
+    let num_words = num_words.get();
 
     let mut rng = OsRng;
 
     let mut rand_word = || loop {
         use rand::Rng;
 
-        const MASK: u16 = (1 << 13) - 1;
-
         let i = usize::from(rng.gen::<u16>() & MASK);
 
-        if let Some(word) = wordlist.get(i) {
+        if let Some(word) = WORDS.get(i) {
             break *word;
         }
     };
 
-    let phrase = (0..args.num_words.get())
+    let phrase = (0..num_words)
         .map(|_| rand_word())
         .collect::<Vec<_>>()
         .join(" ");
 
-    println!("{}", phrase);
+    println!("{phrase}");
 }
